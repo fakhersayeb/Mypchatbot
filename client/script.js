95bb81850dd41533c7a967f512038aa11c075c5c
Fakher Sayeb <fakhersayeb00@gmail.com>
2023-05-26 14:21:09 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://mypchatbot.onrender.com', {
+    const response = await fetch('https://mypchatbot.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
